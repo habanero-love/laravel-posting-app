b85ff701f152d980667033cfac01b5acf53b966a
habanero-love <5kontir0ea@gmail.com>
2024-05-29 02:37:28 +0900

    「Twitter風アプリに機能を追加しよう」関連ファイルを追加・更新

--- laravel-posting-app/database/factories/PostFactory.php
+++ laravel-posting-app/database/factories/PostFactory.php
@@ -18,8 +18,8 @@ class PostFactory extends Factory
     {
         return [
             'user_id' => 1,
-            'title' => fake()->company(),
-            'content' => fake()->realText(),
+            'title' => fake()->realText(20,5),
+            'content' => fake()->realText(200,5),
         ];
     }
 }
